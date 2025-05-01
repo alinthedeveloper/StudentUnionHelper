55cbfd255df6c16ad347eb45412b1d994f7b13cd
Connor Kindall <connorkindall@gmail.com>
2025-04-30 18:15:40 -0600

    arrow
    arrow

--- StudentUnionHelper/ViewModel/ARViewCoordinator.swift
+++ StudentUnionHelper/ViewModel/ARViewCoordinator.swift
@@ -21,7 +21,14 @@ public class Coordinator: NSObject, ARSCNViewDelegate
     public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor)
     {
         guard let _ = anchor as? ARImageAnchor else { return }
-        
+
+        // Add a blue directional arrow above the image anchor
+        let arrowNode = SCNNode()
+        arrowNode.geometry = SCNBox(width: 0.05, height: 0.01, length: 0.2, chamferRadius: 0)
+        arrowNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        arrowNode.position = SCNVector3(0, 0.05, 0)
+        node.addChildNode(arrowNode)
+
         DispatchQueue.main.async {
             let label = UILabel()
             label.text = self.location.name
@@ -30,7 +37,7 @@ public class Coordinator: NSObject, ARSCNViewDelegate
             label.font = .systemFont(ofSize: 24, weight: .bold)
             label.textAlignment = .center
             label.frame = CGRect(x: 20, y: 40, width: 250, height: 50)
-            
+
             if let view = renderer as? ARSCNView {
                 view.addSubview(label)
             }
